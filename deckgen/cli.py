b1ac0d172a8a483f436d383e909f43413282f965
Manuel Gil <manuelgilsitio@gmail.com>
2025-07-22 20:30:39 -0500

    Update

--- deckgen/cli.py
+++ deckgen/cli.py
@@ -1,6 +1,8 @@
 import argparse
 from deckgen.decks.generator import DeckGen
 from deckgen.reader.file_reader import FileReader
+from deckgen.splitter.text_splitter import TextSplitter
+from deckgen.pipelines.qa_pipeline import QAToolKit
 from typing import Optional
 import os
 
@@ -64,10 +66,22 @@ def generate_deck_from_file(
     reader = FileReader(input_file)
     content = reader.get_content()
     print("Content read from file:", content)
+    text_splitter = TextSplitter(document=content)
+    chunks = text_splitter.split_text(
+        method="length", chunk_overlap=100, chunk_size=500
+    )
+
+    print("Content after splitting:", chunks)
+
+    qa_list = []
+    for chunk in chunks:
+        print("Processing chunk:", chunk.get_content())
+        qa_toolkit = QAToolKit(input_text=chunk.get_content())
+        qa_list.extend(qa_toolkit.generate_qa())
 
     deck_gen = DeckGen(input_text=content)
     deck = deck_gen.generate_deck(
-        deck_name=deck_name, deck_description=deck_description
+        qa_list=qa_list, deck_name=deck_name, deck_description=deck_description
     )
 
     print("Generated Deck:", deck.name)
--- deckgen/decks/generator.py
+++ deckgen/decks/generator.py
@@ -19,15 +19,15 @@ class DeckGen:
         self.input_text = input_text
 
     def generate_deck(
-        self, deck_name: Optional[str], deck_description: Optional[str] = None
+        self,
+        qa_list: List[Dict[str, str]],
+        deck_name: Optional[str],
+        deck_description: Optional[str] = None,
     ) -> List[Dict[str, str]]:
         """
         Generates a deck based on the input text.
         :return: List of generated cards. Each card is a dictionary with 'front' and 'back' keys.
         """
-        # Placeholder for deck generation logic
-        qa_toolkit = QAToolKit(input_text=self.input_text)
-        qa_list = qa_toolkit.generate_qa()
         deck = Deck(name=deck_name, description=deck_description)
         for qa in qa_list:
             card = Card(front=qa["question"], back=qa["answer"])
